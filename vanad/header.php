7eb647a6cdb83498fe9a192c2be00fdeb0d5ca33
annaamelkina <anna@ut.ee>
2017-03-19 12:54:20 +0200

    Ymberkorraldused

---
+++ vanad/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Test</title>
+		<meta charset="utf-8">
+	
+		<link rel="stylesheet" type="text/css" href="vorm.css">
+	</head>
+	<body>
---
+++ vanad/ladu.php
@@ -0,0 +1,36 @@
+<?php include("header.php"); ?>
+<?php include("menyy.php"); ?>
+
+	<?php include("menyy.php"); ?>
+		
+		<table>
+			<caption>
+				Laoseis
+			</caption>
+			
+			<tr>
+				<th>Ese</th>
+				<th>Kogus</th>
+				<th>Muuda</th>
+			</tr>
+			
+			<tr>
+				<td>Kylmkapp</td>
+				<td>10</td>
+				<td>&nbsp</td>
+			</tr>
+			
+			<tr>
+				<td>Televiisor</td>
+				<td>20</td>
+				<td>&nbsp</td>
+			</tr>
+			
+			<tr>
+				<td>Tool</td>
+				<td>56</td>
+				<td>&nbsp</td>
+			</tr>
+		</table>
+	<?php include("footer.php"); ?>
+		
