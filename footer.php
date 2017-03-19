cb13221fcad8764f84808486695af1cf5f8f22c6
annaamelkina <anna@ut.ee>
2017-03-19 12:58:17 +0200

    Kustutame failid ka

--- footer.php
+++
@@ -1,6 +0,0 @@
-		<div class="autor">
-			&copy; 2014-2017 Anna Amelkina
-		</div>	
-				
-	</body>
-</html>
--- header.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-	<head>
-		<title>Test</title>
-		<meta charset="utf-8">
-	
-		<link rel="stylesheet" type="text/css" href="vorm.css">
-	</head>
-	<body>
